5bb4f38ecb19b80ee0da9b90d0c736ea34eea409
CuckooLasagne <44742782+CuckooLasagne@users.noreply.github.com>
2025-12-18 20:15:41 +1100

    plz_work

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ customIcon.src = 'Nobel_Prize.png'; // IMAGE PATH
 
 class Reel {
     constructor(x, y, ctx) {
-        this.symbols = ["-", "+", "#", "o", customIcon];
+        this.symbols = ["⏱️", "🕰️", "⏰", "🐰", customIcon];
         this.ctx = ctx;
         this.x = x;
         this.y = y;
